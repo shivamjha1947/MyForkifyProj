de261bf5a037fe3c917224a5f626f8a50868dec3
shivamjha1947 <shivamjha1315@gmail.com>
2023-02-22 15:15:31 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '3630ef9d-a98b-4059-828b-1a6e02982f9c';
+export const MODAL_CLOSE_SEC = 2.5;
